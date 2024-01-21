17b34a55930be1ae4443487da8248b6f6c61972d
atulvinod <atulvinod1911@gmail.com>
2024-01-21 11:07:59 +0530

    commit

--- python-dsa/countNumberOfHousesAtCertainDistance1.py
+++ python-dsa/countNumberOfHousesAtCertainDistance1.py
@@ -2,18 +2,30 @@ from typing import List
 class Solution:
     def countOfPairs(self, n: int, x: int, y: int) -> List[int]:
         result = [0]*n
-        matrix = [[0 for _ in range(n+1)] for _ in range(n+1)]        
+        matrix = [[float('inf') for _ in range(n+1)] for _ in range(n+1)]        
         for i in range(1, n):
             matrix[i][i+1] = 1
             matrix[i+1][i] = 1
             print(matrix)
+        
+        for i in range(1, n+1):
+            matrix[i][i] = 0
+        
+        if x != y:
+            matrix[x][y] = 1
+            matrix[y][x] = 1
         
         for current in range(1, n+1):
             for i in range(1,n+1):
                 for j in range(1, n+1):
                     matrix[i][j] = min(matrix[i][j], matrix[i][current]+matrix[current][j])
 
-        print(matrix)
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if i  == j:
+                    continue
+                result[matrix[i][j]-1] += 1
+        return result
 
 
 
