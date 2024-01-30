581bbd8ef42306846f0fe5195083968062b0b0c8
atulvinod <atulvinod1911@gmail.com>
2024-01-30 12:50:29 +0530

    commit

--- python-dsa/maxAreaInHistogram.py
+++ python-dsa/maxAreaInHistogram.py
@@ -1,35 +1,30 @@
-def solve(arr):
-    previous_smaller_element = [-1]*len(arr)
-    next_smaller_element = [len(arr)]*len(arr)
+def histogramArea(numbers):
     stack = []
+    previousLower = [-1 for _ in range(0, len(numbers))]
+    nextLower = [len(numbers) for _ in range(0, len(numbers))]
     
-    # to calculate the previous smaller element 
-    for i in range(len(arr)):
-        while len(stack) != 0 and  arr[i] < arr[stack[-1]]:
+    for i in range(0, len(numbers)):
+        while len(stack) != 0  and numbers[stack[-1]] > numbers[i]:
             stack.pop()
         if len(stack) != 0:
-            previous_smaller_element[i] = stack[-1] 
+            previousLower[i] = stack[-1]
         stack.append(i)
-    
+            
     stack.clear()
-    # to calculate the next smaller element
-    for i in range(len(arr)):
-        while len(stack) != 0 and arr[i] < arr[stack[-1]]:
-            top_element_index = stack[-1]
-            next_smaller_element[top_element_index] = i
-            stack.pop()
+    for i in range(0, len(numbers)):
+        while len(stack) != 0 and numbers[stack[-1]] > numbers[i]:
+            greaterIndex = stack.pop()
+            nextLower[greaterIndex] = i
         stack.append(i)
         
     area = 0
-    for i in range(len(arr)):
-        cur = (next_smaller_element[i] - previous_smaller_element[i] - 1)*arr[i]
-
+    for i in range(len(numbers)):
+        cur = (nextLower[i] - previousLower[i] - 1)*numbers[i]
         area = max(area,cur)
-    
     return area
         
     
-solve([1,2,1,0,1,1,0,0,2,2,])
+histogramArea([1,2,1,0,1,1,0,0,2,2,])
     
     
     
